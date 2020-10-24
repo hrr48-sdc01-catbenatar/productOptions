1c4540a3228d2b595c8bb2da806588712888f807
Madison Stanford <madison.stanford@gmail.com>
2020-10-24 15:46:57 -0700

    Refactor CRUD to remove redundant code

--- server/database/index.js
+++ server/database/index.js
@@ -4,74 +4,32 @@ const client = new cassandra.Client({ contactPoints: [`${process.env.DB_HOST}:${
 
 module.exports.getOneProduct = (productId, callback) => {
   const query = `SELECT * FROM products WHERE id = ${productId}`;
-  client.execute(query)
-    .then((data) => {
-      callback(null, data);
-    })
-    .catch((err) => {
-      console.error(err);
-      callback(err);
-    });
+  updateDatabase(query, callback);
 }
 
 module.exports.getAllProducts = (callback) => {
   const query = `SELECT * FROM products`;
-  client.execute(query)
-    .then((data) => {
-      callback(null, data);
-    })
-    .catch((err) => {
-      console.error(err);
-      callback(err);
-    });
+  updateDatabase(query, callback);
 }
 
 module.exports.getOneStore = (storeId, callback) => {
   const query = `SELECT * FROM stores WHERE id=${storeId}`;
-  client.execute(query)
-    .then((data) => {
-      callback(null, data);
-    })
-    .catch((err) => {
-      console.error(err);
-      callback(err);
-    });
+  updateDatabase(query, callback);
 }
 
 module.exports.getAllStores = (callback) => {
   const query = `SELECT * FROM stores`;
-  client.execute(query)
-    .then((data) => {
-      callback(null, data);
-    })
-    .catch((err) => {
-      console.error(err);
-      callback(err);
-    });
+  updateDatabase(query, callback);
 }
 
 module.exports.getAllStocks = (callback) => {
   const query = `SELECT * FROM stocks_by_product_id`;
-  client.execute(query)
-    .then((data) => {
-      callback(null, data);
-    })
-    .catch((err) => {
-      console.error(err);
-      callback(err);
-    });
+  updateDatabase(query, callback);
 }
 
 module.exports.getStocksByProductId = (productId, callback) => {
   const query = `SELECT * FROM stocks_by_product_id WHERE productid =${productId}`;
-  client.execute(query)
-    .then((data) => {
-      callback(null, data);
-    })
-    .catch((err) => {
-      console.error(err);
-      callback(err);
-    });
+  updateDatabase(query, callback);
 }
 
 //POST REQUESTS
@@ -79,20 +37,23 @@ module.exports.addProduct = (obj, callback) => {
   const {id, name, price, reviewcount, reviews} = obj;
   const query = `INSERT INTO options.products (id, name, price, reviewcount, reviews)
   VALUES (${id}, '${name}', ${price}, ${reviewcount}, ${reviews}) `;
-  client.execute(query)
-    .then((data) => {
-      callback(null, data);
-    })
-    .catch((err) => {
-      console.error(err);
-      callback(err);
-    });
+  updateDatabase(query, callback);
 }
 
 module.exports.addStore = (obj, callback) => {
   const {id, location} = obj;
   const query = `INSERT INTO options.stores (id, location)
   VALUES (${id}, '${location}') `;
+  updateDatabase(query, callback);
+}
+
+module.exports.addStock = (obj, callback) => {
+  const {productid, id, color, colorurl, location, name, qty, size, storeid} = obj;
+  const query = `INSERT INTO options.stocks_by_product_id (productid, id, color, colorurl, location, name, qty, size, storeid) VALUES (${productid}, ${id}, '${color}', '${colorurl}', '${location}', '${name}', ${qty}, '${size}', ${storeid}) `;
+  updateDatabase(query, callback);
+}
+
+var updateDatabase = function(query, callback) {
   client.execute(query)
     .then((data) => {
       callback(null, data);
@@ -103,15 +64,4 @@ module.exports.addStore = (obj, callback) => {
     });
 }
 
-module.exports.addStock = (obj, callback) => {
-  const {productid, id, color, colorurl, location, name, qty, size, storeid} = obj;
-  const query = `INSERT INTO options.stocks_by_product_id (productid, id, color, colorurl, location, name, qty, size, storeid) VALUES (${productid}, ${id}, '${color}', '${colorurl}', '${location}', '${name}', ${qty}, '${size}', ${storeid}) `;
-  client.execute(query)
-    .then((data) => {
-      callback(null, data);
-    })
-    .catch((err) => {
-      console.error(err);
-      callback(err);
-    });
-}
+
